92d8027da98e4c9af39832c11425fee75a41b874
Sohilhusain86 <ytkaif551@gmail.com>
2026-09-24 16:24:33 +0530

    Delete plans.js

--- api/plans.js
+++
@@ -1,2 +0,0 @@
-const {PLANS}=require('./lib/plans'); const {json,method,error}=require('./lib/http');
-module.exports=async(req,res)=>{try{method(req,'GET');return json(res,200,{ok:true,plans:Object.values(PLANS).map(p=>({...p}))})}catch(e){return error(res,e)}};
